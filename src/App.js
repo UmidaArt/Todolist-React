7608b904cb174816bf28ac2faf7726a805b8de8e
Umidae <umiart55@gmail.com>
2022-05-27 17:02:18 +0600

    corrected some code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
      <BrowserRouter>
          <Routes>
-             <Route path="/todoList" element={<TodoList/>}/>
+             <Route path="/" element={<TodoList/>}/>
          </Routes>
      </BrowserRouter>
   );
